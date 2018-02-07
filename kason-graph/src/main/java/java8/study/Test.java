168193d1bbd4cd2d20ea085fb13d7ee9a707bdc7
kason <zhangkaiseu@163.com>
2018-02-07 23:47:20 +0800

    計算出入度的一個方法 修改

--- kason-graph/src/main/java/java8/study/Test.java
+++ kason-graph/src/main/java/java8/study/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
     public static void main(String[] args) {
         byte l = 112;//byte[0]
-        byte l2 = -88;//byte[1]
+        byte l2 = -88;//byte[最後]
 
         //(l >>> 5) & 1 只要是1 就是邊, 是邊的情況下先<<1 在加上後面的(l2 & 1)來確定是什麼邊（進還是出）
         int r = (((l >>> 5) & 1 ) << 1) + (l2 & 1);
