88760780fb4b98fc2510e88dfdd542be0ad1c8d7
kason <zhangkaiseu@163.com>
2018-03-07 23:01:08 +0800

    使用Mapreduce来实现重建索引

--- kason-graph/src/main/java/org/janusgraph/datacreate/GraphIndexUtils.java
+++ kason-graph/src/main/java/org/janusgraph/datacreate/GraphIndexUtils.java
@@ -1,15 +1,21 @@
 package org.janusgraph.datacreate;
 
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.structure.T;
+import org.apache.tinkerpop.gremlin.structure.Transaction;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.janusgraph.core.*;
 import org.janusgraph.core.schema.JanusGraphIndex;
 import org.janusgraph.core.schema.JanusGraphManagement;
 import org.janusgraph.core.schema.SchemaAction;
 import org.janusgraph.core.schema.SchemaStatus;
+import org.janusgraph.diskstorage.BackendException;
+import org.janusgraph.diskstorage.keycolumnvalue.scan.ScanMetrics;
 import org.janusgraph.graphdb.database.management.ManagementSystem;
 import org.janusgraph.hadoop.MapReduceIndexManagement;
 
+import java.util.Random;
 import java.util.concurrent.ExecutionException;
 
 public class GraphIndexUtils {
@@ -17,9 +23,16 @@ public class GraphIndexUtils {
     private JanusGraph graph = GraphSingle.getGraphSingleInstance().getGraph();
     public static void main(String[] args) {
 
-        new GraphIndexUtils().init();
+        GraphIndexUtils graphIndexUtils = new GraphIndexUtils();
+        //new GraphIndexUtils().init();
         //new GraphIndexUtils().deleteCompositeIndex();
 
+        //new GraphIndexUtils().insert();
+
+        //graphIndexUtils.queryVertex();
+
+        //重建索引数据
+        graphIndexUtils.reindex();
 
     }
 
@@ -73,9 +86,11 @@ public class GraphIndexUtils {
         graph.tx().rollback();
         JanusGraphManagement mgmt = graph.openManagement();
 
-        //VertexLabel person = mgmt.makeVertexLabel("person").make();
-        //EdgeLabel father = mgmt.makeEdgeLabel("fiend").make();
-        PropertyKey rowKey = mgmt.makePropertyKey("rowKey").dataType(String.class).cardinality(Cardinality.SINGLE).make();
+        VertexLabel person = mgmt.makeVertexLabel("person").make();
+        EdgeLabel father = mgmt.makeEdgeLabel("fiend").make();
+        //PropertyKey rowKey = mgmt.makePropertyKey("rowKey").dataType(String.class).cardinality(Cardinality.SINGLE).make();
+        PropertyKey age = mgmt.makePropertyKey("age").dataType(Integer.class).cardinality(Cardinality.SINGLE).make();
+        PropertyKey name = mgmt.makePropertyKey("name").dataType(String.class).cardinality(Cardinality.SINGLE).make();
         //mgmt.buildIndex("rowKey", Vertex.class).addKey(rowKey).buildMixedIndex("search");
         //mgmt.buildIndex("secrowKey", Vertex.class).addKey(rowKey).buildCompositeIndex();
 
@@ -114,8 +129,102 @@ public class GraphIndexUtils {
 
     /**
      * reindex
+     * 首先只针对单个属性建立索引
      */
     public void reindex() {
+        graph.tx().rollback();
+        JanusGraphManagement mgmt = graph.openManagement();
+        //PropertyKey rowKey = graph.getPropertyKey("rowKey");
+        PropertyKey rowKey = mgmt.getPropertyKey("name");
+        mgmt.buildIndex("iname", Vertex.class).addKey(rowKey).buildMixedIndex("search");
+        mgmt.commit();
+        graph.tx().commit();
 
+        graph.tx().rollback();
+        // Block until the SchemaStatus transitions from INSTALLED to REGISTERED
+        try {
+            ManagementSystem.awaitGraphIndexStatus(graph, "iname").call();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Block until the SchemaStatus transitions from INSTALLED to REGISTERED");
+
+        mgmt = graph.openManagement();
+        MapReduceIndexManagement mr = new MapReduceIndexManagement(graph);
+        try {
+            mr.updateIndex(mgmt.getGraphIndex("iname"), SchemaAction.REINDEX).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (BackendException e) {
+            e.printStackTrace();
+        }
+        mgmt.commit();
+        System.out.println("execute reindex with mapreduce");
+        //Enable the index
+        mgmt = graph.openManagement();
+        try {
+            mgmt.updateIndex(mgmt.getGraphIndex("iname"), SchemaAction.ENABLE_INDEX).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        mgmt.commit();
+
+        // Block until the SchemaStatus is ENABLED
+        mgmt = graph.openManagement();
+        try {
+            ManagementSystem.awaitGraphIndexStatus(graph, "iname").status(SchemaStatus.ENABLED).call();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("index update to enable finish");
+        mgmt.commit();
+        graph.close();
+
+    }
+
+    /**
+     * insert data
+     */
+    public void insert() {
+
+        for (int i = 0; i < 100; i++){
+            //JanusGraphManagement janusGraphManagement = graph.openManagement();
+            Transaction tx = graph.tx();
+            for(int j = 0; j < 100; j++){
+                JanusGraphVertex p1 = graph.addVertex("person");
+                p1.property("name", "lily " + System.currentTimeMillis());
+                p1.property("age", new Random().nextInt(20) + 20);
+
+                JanusGraphVertex p2 = graph.addVertex("person");
+                p2.property("name", "smith " + System.currentTimeMillis());
+                p2.property("age", new Random().nextInt(20) + 20);
+
+                p1.addEdge("friend", p2);
+
+            }
+            tx.commit();
+        }
+
+        graph.close();
+    }
+
+    /**
+     * QUERY
+     * 如果不建立索引将会报如下异常
+     * Query requires iterating over all vertices [(age = 25)]. For better performance, use indexes
+     */
+    public void queryVertex(){
+
+        GraphTraversalSource g = graph.traversal();
+        GraphTraversal<Vertex, Vertex> age = g.V().has("age", 25);
+        while (age.hasNext()){
+            System.out.println(age.next());
+        }
+
+        graph.close();
     }
 }
